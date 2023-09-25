ed4f8f978e8501d05a061e3b8e933f00bbfe223c
abinashlingank <abinashlingank@gmail.com>
2023-09-25 22:38:50 +0530

    Add files via upload

---
+++ systemfiles/app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~
+cd /home/Talos/systemT/systemfiles
+python3 /home/Talos/systemT/systemfiles/first.py
+cd
---
+++ systemfiles/launch.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gnome-terminal -- bash -c "/home/Talos/systemT/systemfiles/app.sh; exec bash"
